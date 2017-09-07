bdd0cd5f6c77f8aafeafe4b8c576c5f349d30748
38238-hexaware <31004655+38238-hexaware@users.noreply.github.com>
2017-09-07 17:02:08 +0530

    Update horoscope.js

--- apps/alexatestskill/horoscope.js
+++ apps/alexatestskill/horoscope.js
@@ -1,10 +1,10 @@
-//const request = require('request-sync');
-// let datares;
-// module.exports = function (sign) {
-//     return JSON.parse(request({
-//         uri: "http://widgets.fabulously40.com/horoscope.json?sign=" + sign,
-//         method: 'GET'
-//     }).body);
+const request = require('request-sync');
+let datares;
+module.exports = function (sign) {
+    return JSON.parse(request({
+        uri: "http://widgets.fabulously40.com/horoscope.json?sign=" + sign,
+        method: 'GET'
+    }).body);
 
-// };
-const request = require('request');
+};
+// const request = require('request');
