1159357c7fb3517b444eee281e727e3c5c9743e3
38238-hexaware <31004655+38238-hexaware@users.noreply.github.com>
2017-09-07 16:10:49 +0530

    Update index.js

--- apps/alexatestskill/index.js
+++ apps/alexatestskill/index.js
@@ -3,7 +3,7 @@ module.change_code = 1;
 
 var alexa = require( 'alexa-app' );
 var app = new alexa.app( 'test-skill' );
-const request=new require('request');
+const request=require('request');
 
 
 app.launch( function( request, response ) {
