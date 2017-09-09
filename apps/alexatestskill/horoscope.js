36f2d06b7d95b8ed6e49701c92138f524b9cd3f2
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-09 07:37:35 -0700

    Update horoscope.js

--- apps/alexatestskill/horoscope.js
+++ apps/alexatestskill/horoscope.js
@@ -1,10 +1,52 @@
-const request = require('request-sync');
-let datares;
-module.exports = function (sign) {
-    return JSON.parse(request({
-        uri: "http://widgets.fabulously40.com/horoscope.json?sign=" + sign,
-        method: 'GET'
-    }).body);
+// const request = require('request-sync');
+// let datares;
+// module.exports = function (sign) {
+//     return JSON.parse(request({
+//         uri: "http://widgets.fabulously40.com/horoscope.json?sign=" + sign,
+//         method: 'GET'
+//     }).body);
 
-};
-// const request = require('request');
+// };
+var requestnew = require('request');
+
+module.exports = function(data, callback) {
+
+    var r;
+    var options = {};
+        options.url = "http://widgets.fabulously40.com/horoscope.json?sign="+data;
+        options.method = "GET";
+    requestnew(options, function(error, response, body) {
+
+         if (!error) {
+
+            if(response.statusCode == 200) {
+
+                try {
+
+                    if((typeof body) == "string") {
+
+                        var result = JSON.parse(body);
+
+                        r = result;
+                    } else {
+
+                        r = body;
+                    }
+
+                    // Call callback with no error, and result of request
+                    return callback(null, r);
+
+                } catch (e) {
+
+                    // Call callback with error
+                    return callback(e);
+                }
+            }
+        } else {
+
+            console.log("Error: " + error);
+            return callback(error);
+        }
+    });
+
+}
