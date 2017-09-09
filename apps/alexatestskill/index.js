dfaadd1de1e64e924c29e837d8b6031e9e72629e
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-09 06:49:49 -0700

    Update index.js

--- apps/alexatestskill/index.js
+++ apps/alexatestskill/index.js
@@ -45,7 +45,7 @@ reqnew('http://widgets.fabulously40.com/horoscope.json?sign=capricorn', function
 console.log('error:', error); // Print the error if one occurred 
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
   console.log('body:', body); // Print the HTML for the Google homepage. 
-
+response.say("srini");
 // horoscope=JSON.parse(body);
 // //console.log(horoscope.horoscope.sign+"=>"+horoscope.horoscope.horoscope);
 // sign=horoscope.horoscope.sign;
