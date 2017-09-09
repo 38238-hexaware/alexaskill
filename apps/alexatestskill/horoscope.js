f6dc04c7566cff5fc5957760751776608548a43b
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-09 07:53:07 -0700

    Update horoscope.js

--- apps/alexatestskill/horoscope.js
+++ apps/alexatestskill/horoscope.js
@@ -7,46 +7,46 @@
 //     }).body);
 
 // };
-var requestnew = require('request');
+// var requestnew = require('request');
 
-module.exports = function(data, callback) {
+// module.exports = function(data, callback) {
 
-    var r;
-    var options = {};
-        options.url = "http://widgets.fabulously40.com/horoscope.json?sign="+data;
-        options.method = "GET";
-    requestnew(options, function(error, response, body) {
+//     var r;
+//     var options = {};
+//         options.url = "http://widgets.fabulously40.com/horoscope.json?sign="+data;
+//         options.method = "GET";
+//     requestnew(options, function(error, response, body) {
 
-         if (!error) {
+//          if (!error) {
 
-            if(response.statusCode == 200) {
+//             if(response.statusCode == 200) {
 
-                try {
+//                 try {
 
-                    if((typeof body) == "string") {
+//                     if((typeof body) == "string") {
 
-                        var result = JSON.parse(body);
+//                         var result = JSON.parse(body);
 
-                        r = result;
-                    } else {
+//                         r = result;
+//                     } else {
 
-                        r = body;
-                    }
+//                         r = body;
+//                     }
 
-                    // Call callback with no error, and result of request
-                    return callback(null, r);
+//                     // Call callback with no error, and result of request
+//                     return callback(null, r);
 
-                } catch (e) {
+//                 } catch (e) {
 
-                    // Call callback with error
-                    return callback(e);
-                }
-            }
-        } else {
+//                     // Call callback with error
+//                     return callback(e);
+//                 }
+//             }
+//         } else {
 
-            console.log("Error: " + error);
-            return callback(error);
-        }
-    });
+//             console.log("Error: " + error);
+//             return callback(error);
+//         }
+//     });
 
-}
+// }
