c2bb87e78590465c646d7f0c1fe3fa6e9d48cb5f
srinivasan velu <31004655+38238-hexaware@users.noreply.github.com>
2017-09-10 04:58:06 -0700

    Update index.js

--- apps/alexatestskill/index.js
+++ apps/alexatestskill/index.js
@@ -72,7 +72,7 @@ var zodiac = request.slot('GetZodiacIntent');
 var horoscope,sign,todaysh;
 response.shouldEndSession( false );
 if(zodiac){
-callapi(zodiac, function(err, result1){
+callapi(zodiac, return function(err, result1){
  horoscope=result1;
  sign=horoscope.horoscope.sign;
  todaysh=horoscope.horoscope.horoscope;	
